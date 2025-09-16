3d57378bf609280c3d606e10da50378a56deeb65
pulkitzzz <pulkitgoswami23@gmail.com>
2025-09-16 14:19:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("rgb(165,247,255)");
+fill ("white")
+circle (200,150,110)
+circle (200,100,110)
+fill ("black")
+  circle (180,90,20)
+  fill ("black")
+  circle (220,90,20)
+rect (180,203,10,50 )
+  rect (220,199,10,50 )
+  rect (250,140,50,10 )
+  rect (100,140,50,10 )
+  ellipse(200, 120, 40, 20);
+}
