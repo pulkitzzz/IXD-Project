4ec24dbd0368329beac9a6e0515643cfabccc754
pulkitzzz <pulkitgoswami23@gmail.com>
2025-09-23 02:11:33 +0530

    Update sketch.js
    change to original color

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ function setup() {
 
 function draw() {
   background("rgb(165,247,255)");
-fill ("black")
+fill ("white")
 circle (200,150,110)
 circle (200,100,110)
 fill ("black")
   circle (180,90,20)
-  fill ("white")
+  fill ("black")
   circle (220,90,20)
 rect (180,203,10,50 )
   rect (220,199,10,50 )
